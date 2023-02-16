9d3103f705318f6da889f1af0287e4472ec4b43d
imat19 <vergara.reymart82@gmail.com>
2023-02-17 05:27:18 +0800

    added  single view

--- app/about/errors.jsx
+++ app/about/errors.jsx
@@ -1,10 +1,10 @@
-"use client"
+// "use client"
 
-export default function Error({error, reset}){
-  return(
-    <div>
-      This ain't loading up: {error.message}
-      <button onClick={() => reset()}>Reload</button>
-    </div>
-  )
-}
+// export default function Error({error, reset}){
+//   return(
+//     <div>
+//       This ain't loading up: {error.message}
+//       <button onClick={() => reset()}>Reload</button>
+//     </div>
+//   )
+// }
