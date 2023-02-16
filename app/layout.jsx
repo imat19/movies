059dfe4c8db02f0a0e80eb92f10e9158a68d3962
imat19 <vergara.reymart82@gmail.com>
2023-02-17 02:37:29 +0800

    added about

---
+++ app/layout.jsx
@@ -0,0 +1,35 @@
+import './globals.css'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+
+      <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"/>
+        </head>
+     
+      <body>
+      <nav>
+        <h1>logo</h1>
+        <u>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Sign up</a>
+          </li>
+        </u>
+      </nav>
+      <div className='bg-info'>
+    <page />
+      </div>
+      {children}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        </body>
+    </html>
+  )
+}
