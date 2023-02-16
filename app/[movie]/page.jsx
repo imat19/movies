e81f3674a691d4174c22cdec7cc036559d455449
imat19 <vergara.reymart82@gmail.com>
2023-02-17 05:57:01 +0800

    added index

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -3,7 +3,7 @@ export default async function MovieDetail({ params }) {
   const { movie } = params;
   const imagePath = "https://image.tmdb.org/t/p/original";
   const data = await fetch(
-    `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`
+    `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`, {next: {revalidate :60}}
   )
   const res = await data.json()
   return (
